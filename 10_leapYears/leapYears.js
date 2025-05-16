1319ac8c39f126a46e47a59f5e8ff309c75a018e
Sakolkiat <sakolkiat.n@gmail.com>
2025-05-16 22:56:01 +0700

    Create a function that check leap year

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,19 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    const divisibleByFour = year % 4 == 0;
+    const century = year % 100 == 0;
+    const divisibleByFourHundred = year % 400 == 0;
+
+    if (divisibleByFour &&
+        divisibleByFourHundred || !century
+    ) { return true;} 
+    else {
+        return false;
+    }
 
 };
 
 // Do not edit below this line
 module.exports = leapYears;
+
+
+% 4 == 0
