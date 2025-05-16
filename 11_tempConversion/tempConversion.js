f086ffcd2e49dd79f944f9e7397765f1b60020cf
Sakolkiat <sakolkiat.n@gmail.com>
2025-05-16 23:20:44 +0700

    create temp conversion function

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenheit) {
+    const celsius = ((farenheit - 32) * 5 / 9).toFixed(2);
+    return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  const fahrenheit = ((celcius * 9 / 5) + 32).toFixed(2);
+  return fahrenheit;
 };
 
 // Do not edit below this line
