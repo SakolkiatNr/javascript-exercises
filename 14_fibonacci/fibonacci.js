5420a900c25122f8648151c195d319bd49bec981
Sakolkiat <sakolkiat.n@gmail.com>
2025-05-25 08:45:35 +0700

    updates

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(fn) {
+    let fib = [0, 1];
 
+    for (let i = 2; i <= +fn; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2];
+    }
+    if (+fn < 0) {
+        return 'OOPS';
+    }
+    return fib[+fn];
 };
 
 // Do not edit below this line
