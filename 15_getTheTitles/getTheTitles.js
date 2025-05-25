c2d9a604da3fdd48451fd4c75a55263d0cfc67ef
Sakolkiat <sakolkiat.n@gmail.com>
2025-05-25 09:08:36 +0700

    Add get title function

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+    return arr.reduce((list, value) => {
+        list.push(value.title);
+        return list;
+    } ,[]);
 };
 
 // Do not edit below this line
